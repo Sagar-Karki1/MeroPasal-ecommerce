e2d8a9b69f2fc006815605f94bf641337b50da9f
Sagar-Karki1 <playerc80@gmail.com>
2023-02-09 21:08:58 +0545

    added reducer on feature product

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -3,22 +3,39 @@
 //consumer
 
 import axios from "axios";
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import reducer from "../reducer/ProductReducer";
 
 const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
 
 const AppProvider = ({ children }) => {
+
+  const initialState = {
+    isLoading: false,
+    isError: false,
+    products: [],
+    featureProducts: [],
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   const getProducts = async (url) => {
-    const res = await axios.get(url);
-    const products = await res.data;
-    console.log(products);
+    dispatch({ type: "SET_LOADING"});
+    try{
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({type:"SET_API_DATA", payload: products});
+    } catch (error) {
+      dispatch({type:"API_ERROR"})
+    }
+    
   };
   useEffect(() => {
     getProducts(API);
   }, []);
-  return <AppContext.Provider value="Sagar">{children}</AppContext.Provider>;
+  return <AppContext.Provider value={...state}>{children}</AppContext.Provider>;
 };
 
 //Custom Hook
