8746330675e949513c269f78490c0e492219b97e
Sagar-Karki1 <playerc80@gmail.com>
2023-02-19 19:11:35 +0545

    added product filter

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import ProductList from "../components/ProductList";
+import Sort from "../components/Sort";
+import FilterSection from "../components/FilterSection";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <div className="flex w-full max-w-[1200px] m-auto">
+        <div className="products-filter">
+          <FilterSection />
+        </div>
+        <div className="products-items">
+          <div>
+            <Sort />
+          </div>
+          <div>
+            <ProductList />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Products;
