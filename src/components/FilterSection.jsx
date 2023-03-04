148485d05011f6a0a17e00901d466456f41155ba
Sagar-Karki1 <playerc80@gmail.com>
2023-03-04 21:44:31 +0545

    added filter category wise functionality

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -3,9 +3,20 @@ import { useFilterContext } from "../context/FilterContext";
 
 const FilterSection = () => {
   const {
-    filters: { text },
+    filters: { text, category },
+    all_products,
     updateFilterValue,
   } = useFilterContext();
+
+  //to get the unique data of each field
+  const getUniqueData = (data, property) => {
+    let newValue = data.map((curElem) => {
+      return curElem[property];
+    });
+    return (newValue = ["All", ...new Set(newValue)]); //Set object lets you store unique values of any type
+  };
+  //unique data
+  const categoryOnlyData = getUniqueData(all_products, "category");
   return (
     <>
       <div className="filter-search">
@@ -19,6 +30,25 @@ const FilterSection = () => {
             className="border-2 py-2 px-4 rounded-md"
           />
         </form>
+      </div>
+      <div className="filter-category mt-6">
+        <h3 className="font-semibold text-xl text-gray-700">Categories</h3>
+        <div className="flex flex-col gap-2 flex-start">
+          {categoryOnlyData.map((curElem, index) => {
+            return (
+              <button
+                key={index}
+                type="button"
+                name="category"
+                value={curElem}
+                onClick={updateFilterValue}
+                className="flex capitalize text-gray-400 font-medium"
+              >
+                {curElem}
+              </button>
+            );
+          })}
+        </div>
       </div>
     </>
   );
